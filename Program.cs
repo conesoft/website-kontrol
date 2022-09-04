957c4219fd057dbbfb6efdf14653e80ee5094737
David Spörri <davepermen@hotmail.com>
2022-09-04 19:47:50 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Conesoft.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+var app = builder.Build();
+
+app.UseHostingDefaults(useDefaultFiles: true, useStaticFiles: true);
+app.UseRouting();
+app.MapBlazorHub();
+app.MapRazorPages();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
